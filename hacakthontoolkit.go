be6b6c759dba939c2f61cd61030b0b40ce4dd2fe
SamKimbinyi <skimbinyi@gmail.com>
2019-06-05 15:38:21 +0100

    Added time

--- hacakthontoolkit.go
+++ hacakthontoolkit.go
@@ -2,12 +2,18 @@ package hackathontoolkit
 
 import (
 	"fmt"
-
+	"time"
 	"github.com/go-chat-bot/bot"
 )
 
 func setpizzatime(command *bot.Cmd) (msg string, err error) {
 	msg = fmt.Sprintf("Reminding you to get pizza at %s", command.Args[0])
+	return
+}
+
+func usertime(command *bot.Cmd) (msg string, err error) {
+	t:= time.Now()
+	msg = fmt.Sprintf("%s",t)
 	return
 }
 
@@ -17,5 +23,11 @@ func init() {
 		"Tells you when the next pizza delivery is comming",
 		"",
 		setpizzatime)
+
+		bot.RegisterCommand(
+			"gettime",
+			"Tells you current time",
+			"",
+			usertime)
 
 }
